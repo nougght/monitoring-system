fb3086dd8978ae537bd06db17e0a830abfaaa84d
nougght <muhamedmakusev@gmail.com>
2026-08-20 23:51:24 +0300

    feat(web-panel): add side bar

--- web-panel/src/App.tsx
+++ web-panel/src/App.tsx
@@ -1,7 +1,40 @@
 import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Navigate, Route, Routes, Outlet, useNavigate, useLocation } from 'react-router-dom'
 import { AgentsPage } from './pages/AgentsPage'
 import { NewAgentPage } from './pages/NewAgentPage'
+import { SideBar, type SideBarData, type NavItem } from './components/sideBar'
+import { useEffect, useState } from 'react'
+import dashIcon from "./assets/dashboard.svg"
+import agentsIcon from "./assets/cpu.svg"
+
+const sideBarData: SideBarData = {
+    iconSrc: "",
+    title: "Vigil",
+    items: [
+        { id: "dashboard", title: "Обзор", iconSrc: dashIcon, path: "/dashboard", countLabel: 0 },
+        { id: "agents", title: "Агенты", iconSrc: agentsIcon, path: "/agents", countLabel: 0 },
+    ]
+
+}
+const AppLayout = () => {
+    const navigate = useNavigate()
+    let location = useLocation()
+
+    useEffect(
+        () => {
+
+        },
+        [location]
+    )
+    return (
+        <div style={{ display: `flex`, flexDirection: `row`, height: `100%`, alignItems:`stretch`}}>
+            <SideBar
+                data={sideBarData}
+            />
+            <Outlet/>
+        </div>
+    )
+}
 
 function App() {
 
@@ -9,9 +42,12 @@ function App() {
         <div className='app'>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Navigate to="/agents" replace />} />
-                    <Route path="/agents" element={<AgentsPage />} />
-                    <Route path="/agents/new" element={<NewAgentPage />} />
+                    <Route element={<AppLayout />}>
+                        <Route path="/" element={<Navigate to="/agents" replace />} />
+                        <Route path="/dashboard" element={<p>not implemented</p>} />
+                        <Route path="/agents" element={<AgentsPage />} />
+                        <Route path="/agents/new" element={<NewAgentPage />} />
+                    </Route>
                     {/* <Route path="*" element={<NotFoundPage />} /> */}
                 </Routes>
             </BrowserRouter>
@@ -20,3 +56,4 @@ function App() {
 }
 
 export default App
+
---
+++ web-panel/src/components/sideBar.tsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom"
+
+
+export interface NavItem {
+    id: string
+    title: string
+    iconSrc: string
+    path: string
+    countLabel: number
+}
+
+export interface SideBarData {
+    iconSrc: string
+    title: string
+    items: NavItem[]
+    // TODO: add more sections
+}
+
+interface SideBarProps {
+    data: SideBarData
+    onSwitch: (id: string) => void
+    activeId: string
+}
+
+
+const SideBarButton = ({ data }: { data: NavItem }) => {
+    return (
+        <NavLink
+            key={data.path}
+            to={data.path}
+            className={({ isActive }) => `side-bar-button ${isActive ? "active" : ""}`}
+        >
+            {data.iconSrc != "" && <img src={data.iconSrc} width="15px" height="15px" />}
+            <span className="side-bar-button-title">{data.title}</span>
+            <span className="side-bar-button-count">{data.countLabel}</span>
+        </NavLink>
+    )
+}
+
+export const SideBar = ({ data}: {data: SideBarData}) => {
+    return (
+        <aside className="side-bar">
+            <div className="side-bar-header">
+
+            </div>
+            <nav>
+                {
+                    Array.from(data.items.values()).map((item) => (
+                        <SideBarButton
+                            key={item.id}
+                            data={item}
+                        />
+                    ))
+                }
+            </nav>
+        </aside>
+    )
+}
